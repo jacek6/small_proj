3fdfbea4bdd91713bf3aa4d5f8ea0d3264730cf4
Jacek Skoczylas <gjacek6@gmail.com>
2020-11-10 14:09:24 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='small_proj',
+    version='0.0.1',
+    packages=['example_proj'],
+    url='',
+    license='',
+    author='jacek',
+    author_email='',
+    description='',
+    install_requires=['numpy==1.17.2']
+)
